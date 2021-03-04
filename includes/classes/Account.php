7ccf78ef97d5d58cc334b8b79059ed7f13dfd6f3
Danylo Kazakov <danylokazakov.work@gmail.com>
2021-03-04 21:08:21 +0200

    1.7 Validating name and emails

---
+++ includes/classes/Account.php
@@ -0,0 +1,57 @@
+<?php
+class Account
+{
+    private $errorArray;
+    public function __construct()
+    {
+        $this->errorArray = array();
+    }
+    public function register($un, $fn, $ln, $em, $em2, $pw, $pw2)
+    {
+        $this->validateUsername($un);
+        $this->validateFirstName($fn);
+        $this->validateLastName($ln);
+        $this->validateEmails($em, $em2);
+        $this->validatePasswords($pw, $pw2);
+    }
+    private function validateUsername($un)
+    {
+        if (strlen($un) > 25 ||  strlen($un) < 5) {
+            array_push($this->errorArray, "Your username must be between 5 and 25 characters");
+            return;
+        }
+        //TODO: check if username exists
+    }
+    private function validateFirstName($fn)
+    {
+        if (strlen($fn) > 25 ||  strlen($fn) < 5) {
+            array_push($this->errorArray, "Your first must be between 2 and 25 characters");
+            return;
+        }
+    }
+    private function validateLastName($ln)
+    {
+        if (strlen($ln) > 25 ||  strlen($ln) < 5) {
+            array_push($this->errorArray, "Your last name must be between 2 and 25 characters");
+            return;
+        }
+    }
+    private function validateEmails($em, $em2)
+    {
+        if ($em != $em2) {
+            array_push($this->errorArray, "Your emails don't match");
+            return;
+        }
+        if (!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+            array_push($this->errorArray, "Email is invalid");
+            return;
+        }
+        //TODO: Check that email hasn't been used
+    }
+    private function validatePasswords($pw, $pw2)
+    {
+        if ($pw != $pw2) {
+            array_push($this->errorArray, "Your emails don't match");
+        }
+    }
+}
--- register.php
+++ register.php
@@ -1,7 +1,8 @@
 <?php
 include("includes/classes/Account.php");
+
 $account = new Account();
-$account->register();
+
 include("includes/handlers/register-handler.php");
 include("includes/handlers/login-handler.php");
 ?>
