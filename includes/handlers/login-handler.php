732dca5fe84498b908f538b38367142c3ffca2ef
Danylo Kazakov <danylokazakov.work@gmail.com>
2021-03-04 20:37:54 +0200

    Separating code login and register forms

---
+++ includes/handlers/login-handler.php
@@ -0,0 +1,4 @@
+<?php
+if (isset($_POST['loginButton'])) {
+    //login button was pressed
+}
--- register.php
+++ register.php
@@ -1,42 +1,6 @@
 <?php
-function sanitizeFormUsername($inputText)
-{
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ", "", $inputText);
-    return $inputText;
-}
-function sanitizeFormPassword($inputText)
-{
-    $inputText = strip_tags($inputText);
-    return $inputText;
-}
-function sanitizeFormString($inputText)
-{
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ", "", $inputText);
-    $inputText = ucfirst(strtolower($inputText));
-    return $inputText;
-}
-
-if (isset($_POST['loginButton'])) {
-    //login button was pressed
-}
-if (isset($_POST['registerButton'])) {
-    //login button was pressed
-    $username = sanitizeFormUsername($_POST['username']);
-    $firstName = sanitizeFormString($_POST['firstName']);
-    $lastName = sanitizeFormString($_POST['lastName']);
-    $email = sanitizeFormString($_POST['email']);
-    $email2 = sanitizeFormString($_POST['email2']);
-    $password = sanitizeFormPassword($_POST['password']);
-    $password2 = sanitizeFormPassword($_POST['password2']);
-}
-
-
-
-
+include("includes/handlers/register-handler.php");
 ?>
-
 
 <html>
 
