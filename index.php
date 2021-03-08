9563049b9bb8c43a7ff09290e40eb00dc14272a7
Danylo Kazakov <danylokazakov.work@gmail.com>
2021-03-08 13:34:16 +0200

    Progress bar

--- index.php
+++ index.php
@@ -28,13 +28,24 @@ if (isset($_SESSION['userLoggedIn'])) {
                         <button class="controlButton previus" title="Previous button">
                             <img src="assets/images/icons/previous.png" alt="previus"> </button>
                         <button class="controlButton play" title="Play button">
-                            <img src="assets/images/icons/play.png" alt="play"> </button>
+                            <img src="assets/images/icons/play.png" alt="play">
+                        </button>
                         <button class="controlButton pause" title="Pause button" style="display:none">
-                            <img src="assets/images/icons/pause.png" alt="pause"> </button>
+                            <img src="assets/images/icons/pause.png" alt="pause"></button>
                         <button class="controlButton next" title="Next button">
                             <img src="assets/images/icons/next.png" alt="next"> </button>
                         <button class="controlButton repeat" title="repeat button">
                             <img src="assets/images/icons/repeat.png" alt="repeat"> </button>
+                    </div>
+                    <div class="playbackBar">
+                        <span class="progressTime current">0.00</span>
+                        <div class="progressBar">'
+                            <div class="progressBarBg">
+                                <div class="progress">
+                                </div>
+                            </div>
+                        </div>
+                        <span class="progressTime remaining">0.00</span>
                     </div>
                 </div>
             </div>
