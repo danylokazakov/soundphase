b60756515794414be2c67d22a081961eaf4e64ab
Danylo Kazakov <danylokazakov.work@gmail.com>
2021-03-15 12:48:18 +0200

    Replacing the files

--- index.php
+++ index.php
@@ -1,36 +1,7 @@
-<?php include("includes/header.php"); ?>
-
-<h1 class="pageHeadingBig">You Might Also Like</h1>
-
-<div class="gridViewContainer">
-
-	<?php
-		$albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
-
-		while($row = mysqli_fetch_array($albumQuery)) {
-			
+<?php 
+include("includes/includedFiles.php"); 
+?>
 
 
+<script>openPage("browse.php")</script>
 
-			echo "<div class='gridViewItem'>
-			<a href = 'album.php?id=" . $row['id'] . "'>
-					<img src='" . $row['artworkPath'] . "'>
-
-					<div class='gridViewInfo'>"
-						. $row['title'] .
-					"</div>
-				</a>
-				</div>";
-
-
-
-		}
-	?>
-
-</div>
-
-
-
-
-
-<?php include("includes/footer.php"); ?>
