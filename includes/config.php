5ec5cd57e2917aae4bd7b150a874ea49c3f7b5f6
Danylo Kazakov <danylokazakov.work@gmail.com>
2021-03-15 12:58:20 +0200

    Ajax requests added

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("localhost", "root", "", "slotify");
+	$con = mysqli_connect("localhost", "root", "", "soundphase");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
